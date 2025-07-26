893b510d4dbc848c557f00ac522d9ebbe8315dbf
OnTheRocksEvents <cocktelerianocheydia@gmail.com>
2025-07-26 19:52:27 +0200

    Update sentry.ts

--- src/sentry.ts
+++ src/sentry.ts
@@ -1,8 +1,9 @@
-import * as Sentry from "@sentry/react";
-import { BrowserTracing } from "@sentry/tracing";
+import * as Sentry from '@sentry/react';
+import { BrowserTracing } from '@sentry/tracing';
 
 Sentry.init({
-  dsn: "TU_SENTRY_DSN",
+  dsn: 'TU_SENTRY_DSN_AQUI', // Sustituye por tu DSN real
   integrations: [new BrowserTracing()],
   tracesSampleRate: 1.0,
+  environment: import.meta.env.MODE, // dev, production...
 });
